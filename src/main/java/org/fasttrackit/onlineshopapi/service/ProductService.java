362aecc2163d5de9fce7aa78244f6d8878c23082
Dejmasu Dorian <dejdori95@gmail.com>
2019-04-15 08:17:39 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/onlineshopapi/service/ProductService.java
@@ -0,0 +1,89 @@
+package org.fasttrackit.onlineshopapi.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.fasttrackit.onlineshopapi.domain.Product;
+import org.fasttrackit.onlineshopapi.exception.ResourceNotFoundException;
+import org.fasttrackit.onlineshopapi.persistence.ProductRepository;
+import org.fasttrackit.onlineshopapi.transfer.product.CreateProductRequest;
+import org.fasttrackit.onlineshopapi.transfer.product.GetProductRequest;
+import org.fasttrackit.onlineshopapi.transfer.product.UpdateProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(ProductService.class);
+
+    private final ProductRepository productRepository;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository, ObjectMapper objectMapper) {
+        this.productRepository = productRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    public Product createProduct(CreateProductRequest request) {
+        LOGGER.info("Creating product {}", request);
+        Product product = objectMapper.convertValue(request, Product.class);
+        return productRepository.save(product);
+    }
+
+    public Product getProduct(long id) throws ResourceNotFoundException {
+        LOGGER.info("Retrieving product {}", id);
+        return productRepository.findById(id)
+                // Optional and lambda expression
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Product " + id + " not found"));
+    }
+
+    public Page<Product> getProducts(GetProductRequest request, Pageable pageable) {
+        LOGGER.info("Retrieving products >> {}", request);
+
+        // not an elegant solution, but the easiest one for now
+        if (request.getPartialName() != null &&
+                request.getMinimumQuantity() != null &&
+                request.getMinimumPrice() != null &&
+                request.getMaximumPrice() != null) {
+            return productRepository.findByNameContainingAndPriceBetweenAndQuantityGreaterThanEqual(
+                    request.getPartialName(), request.getMinimumPrice(),
+                    request.getMaximumPrice(), request.getMinimumQuantity(), pageable);
+
+        } else if (request.getMinimumPrice() != null &&
+                request.getMaximumPrice() != null &&
+                request.getMinimumQuantity() != null) {
+            return productRepository.findByPriceBetweenAndQuantityGreaterThanEqual(
+                    request.getMinimumPrice(), request.getMaximumPrice(),
+                    request.getMinimumQuantity(), pageable);
+
+        } else if (request.getPartialName() != null &&
+                request.getMinimumQuantity() != null) {
+            return productRepository.findByNameContainingAndQuantityGreaterThanEqual(
+                    request.getPartialName(), request.getMinimumQuantity(), pageable);
+        }
+
+        return productRepository.findAll(pageable);
+    }
+
+    public Product updateProduct(long id, UpdateProductRequest request) throws ResourceNotFoundException {
+        LOGGER.info("Updating product {}, {}", id, request);
+        Product product = getProduct(id);
+
+        BeanUtils.copyProperties(request, product);
+
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(long id) {
+        LOGGER.info("Deleting product {}", id);
+        productRepository.deleteById(id);
+        LOGGER.info("Deleted product {}", id);
+    }
+}
---
+++ src/main/java/org/fasttrackit/onlineshopapi/transfer/product/customer/UpdateCustomerRequest.java
@@ -0,0 +1,80 @@
+package org.fasttrackit.onlineshopapi.transfer.product.customer;
+
+public class UpdateCustomerRequest {
+
+    private String first_name;
+    private String last_name;
+    private String adress;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "UpdateCustomerRequest{" +
+                "first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", adress='" + adress + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
